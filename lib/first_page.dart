4dc74db408984e4a08a0cb4ad5b6f21277990587
alejandro.soto <soto.its@gmail.com>
2024-09-11 20:49:45 -0600

    11 Sept

---
+++ lib/first_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Aplicación Demo 6"),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Text(
+            "Hola Mundo!!!",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Hola Mundo!!!",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Hola Mundo!!!",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Hola "),
+              Text("que "),
+              Text("hace "),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.star, color: Colors.amber,),
+              Icon(Icons.star, color: Colors.amber,),
+              Icon(Icons.star, color: Colors.amber,),
+              Icon(Icons.star, color: Colors.amber,),
+              Icon(Icons.star_half, color: Colors.amber,),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(onPressed: (){}, child: Text("GUARDAR"),),
+              ElevatedButton(onPressed: (){}, child: Text("MODIFICAR"),),
+              ElevatedButton(onPressed: (){}, child: Text("ELIMINAR"),),
+            ],
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          print("OK");
+        },
+        child: Icon(Icons.add, color: Colors.white,),
+        backgroundColor: Colors.pinkAccent,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,12 @@
+import 'package:demo6/container_page.dart';
+import 'package:demo6/first_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
      title: "Demo6",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Aplicación Demo 6"),
-          backgroundColor: Colors.indigo,
-          foregroundColor: Colors.white,
-        ),
-        body: Column(
-          children: [
-            Text(
-              "Hola Mundo!!!",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Hola Mundo!!!",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Hola Mundo!!!",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Hola "),
-                Text("que "),
-                Text("hace "),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.star, color: Colors.amber,),
-                Icon(Icons.star, color: Colors.amber,),
-                Icon(Icons.star, color: Colors.amber,),
-                Icon(Icons.star, color: Colors.amber,),
-                Icon(Icons.star_half, color: Colors.amber,),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(onPressed: (){}, child: Text("GUARDAR"),),
-                ElevatedButton(onPressed: (){}, child: Text("MODIFICAR"),),
-                ElevatedButton(onPressed: (){}, child: Text("ELIMINAR"),),
-              ],
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: (){
-              print("OK");
-            },
-            child: Icon(Icons.add, color: Colors.white,),
-            backgroundColor: Colors.pinkAccent,
-        ),
-      ),
+      home: ContainerPage()
     )
   );
 }
